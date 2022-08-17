a6c15a8ca1227f1cf38c4ab824ee097d99c84930
crosstyan <crosstyan@outlook.com>
2022-08-17 10:28:11 +0800

    init

---
+++ lib/usful.dart
@@ -0,0 +1,11 @@
+List controlObjects = [
+  {"picture": "images/1.png", "name": "提高风速"},
+  {"picture": "images/2.png", "name": "降低风速"},
+  {"picture": "images/3.png", "name": "最高风速"},
+  {"picture": "images/4.png", "name": "自动"},
+  {"picture": "images/5.png", "name": "门升高"},
+  {"picture": "images/6.png", "name": "门下降"},
+  {"picture": "images/7.png", "name": "门停止"},
+  {"picture": "images/8.png", "name": "开灯"}
+];
+List controlLst = ['doorheight', 'windspeed', 'airvalve', 'ai1', 'ai2', 'light','spray'];
